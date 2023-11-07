0ca0d316101711522228caea69122891e791a05c
totubamowomre <149119116+totubamowomre@users.noreply.github.com>
2023-11-07 09:46:55 +0000

    use email to send FNOL

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class FormComponent implements OnInit {
   @Input() initialData: any;
   form!: FormGroup;
+  isLoading = false;
 
   @Output() emmitter: EventEmitter<any> = new EventEmitter();
 
@@ -29,16 +30,10 @@ export class FormComponent implements OnInit {
 
   onSubmit() {
     if (this.form.valid) {
+      this.isLoading = true;
       this.emmitter.emit(this.form.value);
       console.log('Submitted with value: ' + JSON.stringify(this.form.value));
     }
   }
 
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    if (file) {
-      console.log(file);
-      // Handle the file here.
-    }
-  }
 }
--- src/app/form-page/form-page.component.ts
+++ src/app/form-page/form-page.component.ts
@@ -55,15 +55,40 @@ export class FormPageComponent implements OnInit {
     SessionService.setSessionData(data);
     let fnolId = this.sessionKey;
 
-    this.apiService.updateFnol(fnolId, data).subscribe({
-      next: () => {
-        this.router.navigate(['/confirmation'], { state: { fnolId: fnolId } });
-      },
-      error: (error: any) => {
-        console.error('Error updating Fnol:', error);
-        throw error;
-      },
-    });
+    const emailLink = this.constructEmailLink(data);
+    // Open the default mail client
+    window.open(emailLink, '_blank');
+
+    // Delay the navigation for 2 seconds to give user time to see email client
+    setTimeout(() => {
+      this.apiService.updateFnol(fnolId, data).subscribe({
+        next: () => {
+          this.router.navigate(['/confirmation'], { state: { fnolId: fnolId } });
+        },
+        error: (error: any) => {
+          console.error('Error updating Fnol:', error);
+          throw error;
+        },
+      });
+    }, 2000);
+  }
+
+
+  constructEmailLink(formData: any): string {
+    // You can iterate over formData here to build the body text for the email
+    let emailBody = `Role in Relation to Loss: ${formData.reporter.relationToInsured}\n`;
+    emailBody += `Title: ${formData.reporter.title}\n`;
+    emailBody += `First Name: ${formData.reporter.firstName}\n`;
+    emailBody += `Last Name: ${formData.reporter.lastName}\n`;
+    emailBody += `Phone: ${formData.reporter.phone}\n`;
+    emailBody += `Email: ${formData.reporter.email}\n`;
+    emailBody += `Portal Reference: ${this.sessionKey}\n`;
+
+    // Create the mailto link
+    const subject = encodeURIComponent('FNOL Portal - New Submission');
+    emailBody = encodeURIComponent(emailBody);
+
+    return `mailto:clmins@muninchre.com?subject=${subject}&body=${emailBody}`;
   }
 
   ngOnInit(): void {
