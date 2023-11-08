f3bbebc0c9eb16f29aeada2bf771e2aa7b1dbeca
totubamowomre <149119116+totubamowomre@users.noreply.github.com>
2023-11-08 17:53:36 +0000

    add new fields

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,14 +8,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
   @Input() initialData: any;
-  form!: FormGroup;
+  form: FormGroup;
   isLoading = false;
 
   @Output() emmitter: EventEmitter<any> = new EventEmitter();
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
     this.form = this.formBuilder.group({
       reporter: this.formBuilder.group({
         relationToInsured: ['', Validators.required],
@@ -32,13 +30,53 @@ export class FormComponent implements OnInit {
         postalCode: ['']
       }),
       policy: this.formBuilder.group({
-      }),
-      loss: this.formBuilder.group({
-      }),
-      claimant: this.formBuilder.group({
-      }),
+        policyNumber: ['', Validators.required],
+        contactSameAsReporter: [false, Validators.required],
+        title: [''],
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: [''],
+        phone: [''],
+        addressOne: [''],
+        addressTwo: [''],
+        city: [''],
+        state: [''],
+        country: ['United States'],
+        postalCode: ['']
+      })
+    });
+
+
+    this.form?.get('policy.contactSameAsReporter')?.valueChanges.subscribe((checked) => {
+      if (checked) {
+        this.form.get('policy.title')?.disable();
+        this.form.get('policy.firstName')?.disable();
+        this.form.get('policy.lastName')?.disable();
+        this.form.get('policy.email')?.disable();
+        this.form.get('policy.phone')?.disable();
+        this.form.get('policy.addressOne')?.disable();
+        this.form.get('policy.addressTwo')?.disable();
+        this.form.get('policy.city')?.disable();
+        this.form.get('policy.state')?.disable();
+        this.form.get('policy.country')?.disable();
+        this.form.get('policy.postalCode')?.disable();
+      } else {
+        this.form.get('policy.title')?.enable();
+        this.form.get('policy.firstName')?.enable();
+        this.form.get('policy.lastName')?.enable();
+        this.form.get('policy.email')?.enable();
+        this.form.get('policy.phone')?.enable();
+        this.form.get('policy.addressOne')?.enable();
+        this.form.get('policy.addressTwo')?.enable();
+        this.form.get('policy.city')?.enable();
+        this.form.get('policy.state')?.enable();
+        this.form.get('policy.country')?.enable();
+        this.form.get('policy.postalCode')?.enable();
+      }
     });
   }
+
+  ngOnInit(): void { }
 
   onSubmit() {
     if (this.form.valid) {
